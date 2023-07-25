be381731830b1ee434765f0499b442ca53e639f5
MURLI PATEL <100035961+murlipatel1@users.noreply.github.com>
2023-07-25 07:32:56 +0530

    Update firstapp.java

--- JAVAFX/firstapp.java
+++ JAVAFX/firstapp.java
@@ -1,1 +1,19 @@
-
+import javafx.application.Application;  
+import javafx.scene.Scene;  
+import javafx.scene.control.Button;  
+import javafx.stage.Stage;  
+import javafx.scene.layout.StackPane;  
+public class Hello_World extends Application{  
+  
+    @Override  
+    public void start(Stage primaryStage) throws Exception {  
+        Button btn1=new Button("Say, Hello World");  
+        StackPane root=new StackPane();  
+        root.getChildren().add(btn1);  
+        Scene scene=new Scene(root);      
+        primaryStage.setScene(scene);  
+        primaryStage.setTitle("First JavaFX Application");  
+        primaryStage.show();  
+    }  
+  
+}  
